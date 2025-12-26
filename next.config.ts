b9308a8c4dc1a358ab9c1f2d33691d6c908582da
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-12-26 11:10:11 +0700

    feat: add show user name

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api-beta.aicademy.org/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
